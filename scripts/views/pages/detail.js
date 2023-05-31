653ed13d465ca398a582e51546aea930354bc0a3
monica <monicaaudry90@gmail.com>
2023-05-31 18:34:21 +0700

    first commit

---
+++ scripts/views/pages/detail.js
@@ -0,0 +1,35 @@
+import UrlParser from '../../routes/url-parser';
+import restaurantSource from '../../data/restaurant-source';
+import { createrestaurantsDetailTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+
+const DetailRestaurant = {
+  async render() {
+    return `
+        <div id="detailRestaurant" class="detailRestaurant"></div>
+        <div id="likeButtonContainer"></div>
+        `;
+  },
+
+  async afterRender() {
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurant = await restaurantSource.detailRestaurant(url.id);
+    const restaurantContainer = document.querySelector('#detailRestaurant');
+
+    restaurantContainer.innerHTML = createrestaurantsDetailTemplate(restaurant);
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      Restaurant: {
+        id: restaurant.id,
+        name: restaurant.name,
+        city: restaurant.city,
+        description: restaurant.description,
+        pictureId: restaurant.pictureId,
+        rating: restaurant.rating,
+      },
+    });
+  },
+};
+
+export default DetailRestaurant;
---
+++ scripts/views/pages/home.js
@@ -0,0 +1,25 @@
+import restaurantSource from '../../data/restaurant-source';
+import { createrestaurantsItemTemplate } from '../templates/template-creator';
+
+const Home = {
+  async render() {
+    return `
+    <section class="content">
+      <div class="latest">
+        <h1>Explore Restaurant</h1>
+        <div class="list" id="tes"></div>
+      </div>
+    </section>
+      `;
+  },
+
+  async afterRender() {
+    const restaurant = await restaurantSource.listRestaurans();
+    const restaurantContainer = document.querySelector('#tes');
+    restaurant.forEach((restaurants) => {
+      restaurantContainer.innerHTML += createrestaurantsItemTemplate(restaurants);
+    });
+  },
+};
+
+export default Home;
